4f6ee368f043a65c248b5c1d9f9d74fb6eb8128b
rushikesh420 <rushikeshnarhare17@gmail.com>
2020-08-10 23:13:38 -0500

    Updates

---
+++ precache-manifest.41430f9bb8e96d5c80be049e79ddde77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e607f88656b9df34854b1699dfe0c4",
+    "url": "/github.io/crypto-pricing/index.html"
+  },
+  {
+    "revision": "10202a3614134951f7a7",
+    "url": "/github.io/crypto-pricing/static/css/main.9b6e2057.chunk.css"
+  },
+  {
+    "revision": "dd08f45fca40d1bec196",
+    "url": "/github.io/crypto-pricing/static/js/2.d1ac3639.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/crypto-pricing/static/js/2.d1ac3639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10202a3614134951f7a7",
+    "url": "/github.io/crypto-pricing/static/js/main.273e626b.chunk.js"
+  },
+  {
+    "revision": "120a9d93555e6dcfc2bf",
+    "url": "/github.io/crypto-pricing/static/js/runtime-main.94ba3209.js"
+  }
+]);
